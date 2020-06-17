41794337b18008ae9073cc52058e888e11382324
zhangxing <zhangxing@diligrp.com>
2020-06-17 11:28:36 +0800

    customer

---
+++ account-service/src/main/java/com/dili/account/dto/CustomerQuery.java
@@ -0,0 +1,26 @@
+package com.dili.account.dto;
+
+/**
+ * <B>Description</B>
+ * <B>Copyright:本软件源代码版权归农丰时代所有,未经许可不得任意复制与传播.</B>
+ * <B>农丰时代科技有限公司</B>
+ *
+ * @author yuehongbo
+ * @date 2020/2/5 14:14
+ */
+public class CustomerQuery{
+
+	/**
+	 * 客户ID
+	 */
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
---
+++ account-service/src/main/java/com/dili/account/dto/PayAccountDto.java
@@ -0,0 +1,19 @@
+package com.dili.account.dto;
+
+
+/**
+ * 支付账户信息
+ * @author apache
+ */
+public class PayAccountDto {
+	/** 支付账户余额 */
+	private Long balance;
+
+	public Long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Long balance) {
+		this.balance = balance;
+	}
+}
