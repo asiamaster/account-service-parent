166be8f27aff4ac809a52b0940b968e8f2d2b396
zhangxing <zhangxing@diligrp.com>
2020-06-18 09:41:01 +0800

    return card api

--- account-service/src/main/java/com/dili/account/controller/CardManageController.java
+++ account-service/src/main/java/com/dili/account/controller/CardManageController.java
@@ -1,85 +1,34 @@
 package com.dili.account.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dili.account.dto.CardRequestDto;
+import com.dili.account.service.ICardManageService;
+import com.dili.ss.domain.BaseOutput;
+
 /**
  * 卡片管理服务，退卡、挂失、解挂、补卡等
- * 
  * @author ：WangBo
  * @time ：2020年4月28日下午4:04:46
  */
 @RestController
-@RequestMapping(value = "user")
+@RequestMapping(value = "/account")
 public class CardManageController {
 
-//	@Resource
-//	private ICardManageService cardManageService;
-//
-//	/**
-//	 * 退卡
-//	 */
-//	@PostMapping("returnCard")
-//	public Message<?> returnCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		AssertUtils.notEmpty(cardParam.getLoginPwd(),"登录密码不能为空!");
-//		cardManageService.returnCard(cardParam);
-//		return Message.success();
-//	}
-//
-//	/**
-//	 * 换卡
-//	 */
-//	@PostMapping("changeCard")
-//	public Message<?> changeCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		AssertUtils.notEmpty(cardParam.getLoginPwd(),"登录密码不能为空!");
-//		AssertUtils.notEmpty(cardParam.getNewCardNo(),"新卡号不能为空!");
-//		cardManageService.changeCard(cardParam);
-//		return Message.success();
-//	}
-//
-//	/**
-//	 * 补卡
-//	 */
-//	@PostMapping("reissueCard")
-//	public Message<?> reissueCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		AssertUtils.notEmpty(cardParam.getLoginPwd(),"登录密码不能为空!");
-//		AssertUtils.notEmpty(cardParam.getNewCardNo(),"新卡号不能为空!");
-//		cardManageService.reissueCard(cardParam);
-//		return Message.success();
-//	}
-//
-//	/**
-//	 * 挂失卡片
-//	 */
-//	@PostMapping("lostCard")
-//	public Message<?> lostCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		AssertUtils.notEmpty(cardParam.getLoginPwd(),"登录密码不能为空!");
-//		cardManageService.lostCard(cardParam.getAccountId(), cardParam.getLoginPwd());
-//		return Message.success();
-//	}
-//
-//	/**
-//	 * 解挂卡片
-//	 */
-//	@PostMapping("unLostCard")
-//	public Message<?> unLostCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		AssertUtils.notEmpty(cardParam.getLoginPwd(),"登录密码不能为空!");
-//		cardManageService.unLostCard(cardParam.getAccountId(), cardParam.getLoginPwd());
-//		return Message.success();
-//	}
-//	
-//	/**
-//	 * 解锁卡片
-//	 */
-//	@PostMapping("unLockCard")
-//	public Message<?> unLockCard(@RequestBody CardManageParamDto cardParam) {
-//		AssertUtils.notNull(cardParam.getAccountId(),"卡账号不能为空!");
-//		cardManageService.unLock(cardParam.getAccountId(), cardParam.getLoginPwd());
-//		return Message.success();
-//	}
+	@Resource
+	private ICardManageService cardManageService;
+
+	/**
+	 * 退卡
+	 */
+	@PostMapping("/returnCard")
+	public BaseOutput<Boolean> returnCard(@RequestBody CardRequestDto cardRequest) {
+		cardManageService.returnCard(cardRequest);
+		return BaseOutput.success();
+	}
 }
