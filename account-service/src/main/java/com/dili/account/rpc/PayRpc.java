baa5bfe869cbcd4e503a55e30790fe4f69c74f00
wangbo <wangbo@diligrp.com>
2020-06-23 16:35:17 +0800

    去掉错误代码

--- account-service/src/main/java/com/dili/account/rpc/PayRpc.java
+++ account-service/src/main/java/com/dili/account/rpc/PayRpc.java
@@ -1,13 +1,26 @@
 package com.dili.account.rpc;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dili.account.dto.FundAccountDto;
+import com.dili.ss.domain.BaseOutput;
 
 /**
- *
- *支付远程调用 相关方法  充值 扣款 查询余额等
- *
+ * @description： 
+ *          支付服务调用
+ * @author ：WangBo
+ * @time ：2020年6月22日下午5:52:52
  */
-@FeignClient(value = "pay-service")
+@FeignClient(value = "dili-pay")
 public interface PayRpc {
-
+	
+	/**
+     * 创建资金账户服务
+     * @param type
+     * @return
+     */
+    @RequestMapping(value = "/api/createFundAccount", method = RequestMethod.POST)
+    BaseOutput<String> createFundAccount(FundAccountDto type);
 }
--- account-service/src/main/java/com/dili/account/service/impl/OpenCardServiceImpl.java
+++ account-service/src/main/java/com/dili/account/service/impl/OpenCardServiceImpl.java
@@ -7,30 +7,47 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dili.account.common.BizNoServiceType;
+import com.dili.account.dto.FundAccountDto;
 import com.dili.account.dto.OpenCardDto;
 import com.dili.account.dto.OpenCardResponseDto;
 import com.dili.account.dto.UserAccountCardQuery;
 import com.dili.account.dto.UserAccountCardResponseDto;
 import com.dili.account.entity.CardStorageDo;
+import com.dili.account.entity.UserAccountDo;
+import com.dili.account.entity.UserCardDo;
 import com.dili.account.exception.AccountBizException;
+import com.dili.account.rpc.UidRpc;
+import com.dili.account.rpc.resolver.PayRpcResolver;
+import com.dili.account.rpc.resolver.UidRpcResovler;
 import com.dili.account.service.IAccountQueryService;
 import com.dili.account.service.ICardStorageService;
 import com.dili.account.service.IOpenCardService;
+import com.dili.account.type.AccountType;
+import com.dili.account.type.AccountUsageType;
+import com.dili.account.type.CardType;
+import com.dili.account.type.CustomerType;
+import com.dili.account.type.UsePermissionType;
+import com.dili.ss.domain.BaseOutput;
 import com.google.common.collect.Lists;
 
 /**
- * @description： 
- *           开卡service实现
+ * @description： 开卡service实现
+ * 
  * @author ：WangBo
  * @time ：2020年6月19日下午5:54:23
  */
 @Service("openCardService")
-public class OpenCardServiceImpl implements IOpenCardService{
+public class OpenCardServiceImpl implements IOpenCardService {
 
 	@Resource
 	private ICardStorageService ICardStorageService;
 	@Resource
 	private IAccountQueryService accountQueryService;
+	@Resource
+	private UidRpcResovler uidRpcResovler;
+	@Resource
+	private PayRpcResolver payRpcResolver;
 //	@Resource
 //	private IUserAccountDao userAccountDao;
 //	@Resource
@@ -45,9 +62,7 @@ public class OpenCardServiceImpl implements IOpenCardService{
 //	private CrmRpc crmRpc;
 //	@Resource
 //	private PayRpc payRpc;
-	
-	
-	
+
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public OpenCardResponseDto openMasterCard(OpenCardDto openCardInfo) {
@@ -58,7 +73,7 @@ public class OpenCardServiceImpl implements IOpenCardService{
 		}
 		// 判断客户是否已办理过主卡，寿光每个人只能有一张交易主卡,其它市场允许办两张卡，则判断只能一张交易买家卡和一张交易卖家卡
 		UserAccountCardQuery queryParam = new UserAccountCardQuery();
-		queryParam.setCustomerIds(Lists.newArrayList(openCardInfo.getCustormerId()));
+		queryParam.setCustomerIds(Lists.newArrayList(openCardInfo.getCustomerId()));
 		List<UserAccountCardResponseDto> userAccountList = accountQueryService.getListByConditionForRest(queryParam);
 		if (userAccountList.size() > 0) {
 			// TODO 是否允许两张卡
@@ -66,27 +81,19 @@ public class OpenCardServiceImpl implements IOpenCardService{
 		}
 		// 判断卡类型，并将卡片改为使用中
 		CardStorageDo cardStorageDo = ICardStorageService.inUse(openCardInfo.getCardNo());
-//		if (cardStorageDo.getType() != CardCategory.MASTER.getCode()) {
-//			throw new AccountBizException("该卡{}不是主卡，操作失败!", openCardInfo.getCardNo());
-//		}
-//
-//		// 对公户保存法人信息
-//		UserLegalEntity userLegal = new UserLegalEntity();
-//		if (AccountType.PUBLIC.getCode() == openCardInfo.getAccountType()) {
-//			userLegal = buildUserLegal(openCardInfo);
-//			userLegalDao.save(userLegal);
-//		}
-//		// 保存用户信息，如果已有用户信息则使用原信息
-//		UserInfoEntity queryParam = new UserInfoEntity();
-//		queryParam.setCrmCustormerId(openCardInfo.getCrmCustormerId());
-//		UserInfoEntity userInfo = userInfoDao.getOnlyOne(queryParam);
-//		if (userInfo == null) {
-//			userInfo = buildUserInfo(openCardInfo);
-//			userInfo.setLegalId(userLegal.getId());
-//			userInfoDao.save(userInfo);
-//		}
-//		// 构建账户信息
-//		UserAccountEntity userAccount = buildUserAccount(openCardInfo, userInfo.getId());
+		if (cardStorageDo.getType() != CardType.MASTER.getCode()) {
+			throw new AccountBizException("该卡{}不是主卡，操作失败!", openCardInfo.getCardNo());
+		}
+
+		// 创建资金账户
+		FundAccountDto fundAccount = new FundAccountDto();
+		String fundAccountIdStr = payRpcResolver.createFundAccount(fundAccount);
+		Long fundAccountId = Long.parseLong(fundAccountIdStr);
+
+		// 构建账户信息
+//		String accountIdStr = uidRpcResovler.bizNumberRetry(BizNoServiceType.ACCOUNT_ID, 3);
+//		Long accountId = Long.parseLong(accountIdStr);
+//		UserAccountDo userAccount = buildUserAccount(openCardInfo, accountId, fundAccountId);
 //
 //		// 保存卡片信息
 //		UserCardEntity userCard = buildUserCard(openCardInfo, userAccount.getId());
@@ -99,11 +106,10 @@ public class OpenCardServiceImpl implements IOpenCardService{
 //		// 保存使用额度权限
 //		UserAmountLimitEntity userLimit = buildUserLimit(openCardInfo, userAccount.getId());
 //		amountLimitService.save(userLimit);
-//
-//		// 返回数据
-//		OpenCardResponseDto response = new OpenCardResponseDto();
+
+		// 返回数据
+		OpenCardResponseDto response = new OpenCardResponseDto();
 //		response.setAccountId(userAccount.getId());
-//		return response;
 		return null;
 	}
 
@@ -112,6 +118,7 @@ public class OpenCardServiceImpl implements IOpenCardService{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 //
 //	@Override
 //	public List<UserInfoEntity> listUser() {
@@ -252,18 +259,34 @@ public class OpenCardServiceImpl implements IOpenCardService{
 //		return userInfo;
 //	}
 //
-//	/**
-//	 * 构建卡账户数据
-//	 *
-//	 * @throws Exception
-//	 */
-//	private UserAccountEntity buildUserAccount(OpenCardDto openCardInfo, Long userInfoId) {
-//		UserAccountEntity userAccount = new UserAccountEntity();
-//		IKeyGenerator userAccountKey = keyGeneratorManager.getKeyGenerator(SequenceKey.USER_ACCOUNT);
-//		Long accountId = userAccountKey.nextId();
-//		userAccount.setId(accountId);
-//		userAccount.setUserInfoId(userInfoId);
-//		userAccount.setCrmCustormerId(openCardInfo.getCrmCustormerId());
+	/**
+	 * 构建卡账户数据
+	 *
+	 * @throws Exception
+	 */
+	private UserAccountDo buildUserAccount(OpenCardDto openCardInfo, Long accountId, Long fundAccountId) {
+		UserAccountDo userAccount = new UserAccountDo();
+		userAccount.setAccountId(accountId);
+		userAccount.setParentAccountId(openCardInfo.getParentAccountId());
+		userAccount.setCustomerId(openCardInfo.getCustomerId());
+		userAccount.setFundAccountId(fundAccountId);
+
+		userAccount.setUsageType(openCardInfo.getUsageType());
+
+		if (CustomerType.PURCHASE.getCode().equalsIgnoreCase(openCardInfo.getType())) {
+			userAccount.setType(AccountType.PURCHASE.getCode());
+			userAccount.setUsageType(AccountUsageType.TRADE.getCode());
+			Integer[] codes = { UsePermissionType.RECHARGE.getCode(), 
+								UsePermissionType.WEALTH.getCode(),
+								UsePermissionType.TRANSACTION.getCode(),
+								UsePermissionType.PAY_FEES.getCode()};
+			userAccount.setPermissions(UsePermissionType.getPermissions(codes));
+		} else if (CustomerType.SALE.getCode().equalsIgnoreCase(openCardInfo.getType())) {
+			
+		} else if (CustomerType.DRIVER.getCode().equalsIgnoreCase(openCardInfo.getType())) {
+
+		}
+
 //		userAccount.setAccountType(openCardInfo.getCustormerType());
 //		userAccount.setBizUsageType(openCardInfo.getBizUsageType());
 //		userAccount.setCreateSource(openCardInfo.getCreateSource());
@@ -275,14 +298,14 @@ public class OpenCardServiceImpl implements IOpenCardService{
 //		LocalDateTime now = LocalDateTime.now();
 //		userAccount.setModifiedTime(now);
 //		userAccount.setCreatedTime(now);
-//		return userAccount;
-//	}
-//
-//	/**
-//	 * 构建卡片数据
-//	 */
-//	private UserCardEntity buildUserCard(OpenCardDto openCardInfo, Long userAccountId) {
-//		UserCardEntity userCard = new UserCardEntity();
+		return userAccount;
+	}
+
+	/**
+	 * 构建卡片数据
+	 */
+	private UserCardDo buildUserCard(OpenCardDto openCardInfo, Long userAccountId) {
+		UserCardDo userCard = new UserCardDo();
 //		IKeyGenerator userCardKey = keyGeneratorManager.getKeyGenerator(SequenceKey.USER_CARD);
 //		userCard.setId(userCardKey.nextId());
 //		userCard.setAccountId(userAccountId);
@@ -299,40 +322,6 @@ public class OpenCardServiceImpl implements IOpenCardService{
 //		userCard.setCreatedTime(now);
 //		userCard.setModifiedTime(now);
 //		userCard.setSystemDisableStatus(SystemDisableStatus.ENABLED.getCode());
-//		return userCard;
-//	}
-//
-//	/**
-//	 * 构建法人信息
-//	 */
-//	private UserLegalEntity buildUserLegal(OpenCardDto openCardInfo) {
-//		UserLegalEntity userLegal = new UserLegalEntity();
-//		IKeyGenerator userLegalKey = keyGeneratorManager.getKeyGenerator(SequenceKey.USER_LEGAL);
-//		Long userLegalId = userLegalKey.nextId();
-//		userLegal.setId(userLegalId);
-//		userLegal.setLegalCredentialType(openCardInfo.getLegalCredentialType());
-//		userLegal.setLegalName(openCardInfo.getLegalName());
-//		userLegal.setLegalNo(openCardInfo.getLegalNo());
-//		LocalDateTime now = LocalDateTime.now();
-//		userLegal.setCreatedTime(now);
-//		userLegal.setModifiedTime(now);
-//		return userLegal;
-//	}
-//
-//	/**
-//	 * 构建用户权限数据
-//	 * 
-//	 * @param openCardInfo 开卡信息
-//	 * @param accountId    账户ID
-//	 */
-//	private UserAmountLimitEntity buildUserLimit(OpenCardDto openCardInfo, long accountId) {
-//		UserAmountLimitEntity userLimit = new UserAmountLimitEntity();
-//		userLimit.setAccountId(accountId);
-//		userLimit.setCardCategory(openCardInfo.getCategory());
-//		userLimit.setCashOutLimitsDays(openCardInfo.getCashOutLimitsDays());
-//		userLimit.setCashOutLimitsTimes(openCardInfo.getCashOutLimitsTimes());
-//		userLimit.setPayLimitsDays(openCardInfo.getPayLimitsDays());
-//		userLimit.setPayLimitsTimes(openCardInfo.getPayLimitsTimes());
-//		return userLimit;
-//	}
+		return userCard;
+	}
 }
