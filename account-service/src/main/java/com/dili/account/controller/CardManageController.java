49302f3aa9d8c893896a71b0b999cf781451126e
zhangxing <zhangxing@diligrp.com>
2020-07-23 15:12:01 +0800

    删除不必要的类

--- account-service/src/main/java/com/dili/account/controller/CardManageController.java
+++ account-service/src/main/java/com/dili/account/controller/CardManageController.java
@@ -1,19 +1,19 @@
 package com.dili.account.controller;
 
-import cn.hutool.core.util.StrUtil;
-import com.dili.account.dto.CardRequestDto;
-import com.dili.account.service.ICardManageService;
-import com.dili.account.validator.CardValidator;
-import com.dili.ss.domain.BaseOutput;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.annotation.Resource;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import com.dili.account.dto.CardRequestDto;
+import com.dili.account.service.ICardManageService;
+import com.dili.account.validator.CardValidator;
+import com.dili.ss.domain.BaseOutput;
+
+import cn.hutool.core.util.StrUtil;
 
 /**
  * 卡片管理服务，退卡、挂失、解挂、补卡等
