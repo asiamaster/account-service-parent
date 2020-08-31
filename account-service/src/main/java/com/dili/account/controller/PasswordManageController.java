f31db2bdae99c4ab45e853b2d07b6045b646b544
wangbo <wangbo@diligrp.com>
2020-08-31 15:52:32 +0800

    add log

--- account-service/src/main/java/com/dili/account/controller/PasswordManageController.java
+++ account-service/src/main/java/com/dili/account/controller/PasswordManageController.java
@@ -1,5 +1,7 @@
 package com.dili.account.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
 import com.dili.account.dto.CardRequestDto;
 import com.dili.account.service.IPasswordService;
 import com.dili.account.validator.CardValidator;
@@ -19,25 +22,32 @@ import com.dili.ss.domain.BaseOutput;
 @RequestMapping("/api/card")
 public class PasswordManageController {
 
-    @Autowired
-    private IPasswordService passwordService;
+	private static final Logger log = LoggerFactory.getLogger(PasswordManageController.class);
 
-    /**
-     * 重置登陆密码
-     */
-    @RequestMapping(value = "/resetLoginPwd", method = RequestMethod.POST)
-    public BaseOutput<Boolean> resetLoginPassword(@RequestBody @Validated(value = {CardValidator.Generic.class, CardValidator.ResetPassword.class}) CardRequestDto cardRequest) throws Exception {
-        passwordService.resetLoginPwd(cardRequest);
-        return BaseOutput.success();
-    }
-    
-    /**
-     * 校验密码
-     */
-    @RequestMapping(value = "/checkPassword", method = RequestMethod.POST)
-    public BaseOutput<Boolean> checkPassword(@RequestBody @Validated(value = {CardValidator.Generic.class}) CardRequestDto cardRequest) throws Exception {
-        passwordService.checkPassword(cardRequest.getAccountId(), cardRequest.getLoginPwd());
-        return BaseOutput.success();
-    }
+	@Autowired
+	private IPasswordService passwordService;
+
+	/**
+	 * 重置登陆密码
+	 */
+	@RequestMapping(value = "/resetLoginPwd", method = RequestMethod.POST)
+	public BaseOutput<Boolean> resetLoginPassword(@RequestBody @Validated(value = { CardValidator.Generic.class,
+			CardValidator.ResetPassword.class }) CardRequestDto cardRequest) throws Exception {
+		log.info("重置登陆密码>>>>" + JSONObject.toJSONString(cardRequest));
+		passwordService.resetLoginPwd(cardRequest);
+		return BaseOutput.success();
+	}
+
+	/**
+	 * 校验密码
+	 */
+	@RequestMapping(value = "/checkPassword", method = RequestMethod.POST)
+	public BaseOutput<Boolean> checkPassword(
+			@RequestBody @Validated(value = { CardValidator.Generic.class }) CardRequestDto cardRequest)
+			throws Exception {
+		log.info("校验登陆密码>>>>" + JSONObject.toJSONString(cardRequest));
+		passwordService.checkPassword(cardRequest.getAccountId(), cardRequest.getLoginPwd());
+		return BaseOutput.success();
+	}
 
 }
