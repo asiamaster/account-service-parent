589f7e9411f2b8db8ab55ac5bad0178c548ca9fd
miaoguoxin <398544597@qq.com>
2020-09-18 12:37:04 +0800

    null

--- account-service/src/main/java/com/dili/account/common/handler/IControllerHandler.java
+++ account-service/src/main/java/com/dili/account/common/handler/IControllerHandler.java
@@ -18,36 +18,39 @@ import java.util.Arrays;
 public interface IControllerHandler {
 
     /**
-    *
-    * @author miaoguoxin
-    * @date 2020/9/18
-    */
-    default void buildFirmId(BaseDto baseDto){
+     *
+     * @author miaoguoxin
+     * @date 2020/9/18
+     */
+    default void buildFirmId(BaseDto baseDto) {
         Long firmId = getFirmId();
-        if (firmId == null){
+        if (firmId == null) {
             //如果header里面拿不到，再检验是否已经传了firmId
-            AssertUtils.notNull(baseDto.getFirmId(),"市场id不能为空");
-        }else {
+            AssertUtils.notNull(baseDto.getFirmId(), "市场id不能为空");
+        } else {
             baseDto.setFirmId(firmId);
         }
     }
 
     /**
-    *
-    * @author miaoguoxin
-    * @date 2020/9/18
-    */
-   default Long getFirmId(){
-       RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-       if (requestAttributes == null) {
-           return null;
-       }
-       HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-       Cookie[] cookies = request.getCookies();
-       return Arrays.stream(cookies)
-               .filter(cookie -> Constant.UAP_FIRMID.equals(cookie.getName()))
-               .findFirst()
-               .map(c -> Long.valueOf(c.getValue()))
-               .orElse(null);
-   }
+     *
+     * @author miaoguoxin
+     * @date 2020/9/18
+     */
+    default Long getFirmId() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null || cookies.length == 0) {
+            return null;
+        }
+        return Arrays.stream(cookies)
+                .filter(cookie -> Constant.UAP_FIRMID.equals(cookie.getName()))
+                .findFirst()
+                .map(c -> Long.valueOf(c.getValue()))
+                .orElse(null);
+    }
 }
