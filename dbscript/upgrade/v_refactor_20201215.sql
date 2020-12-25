754cac7d5e869c36204a107b14baba27776118da
wangbo <wangbo@diligrp.com>
2020-12-25 10:52:17 +0800

    脚本调整

--- dbscript/upgrade/v_refactor_20201215.sql
+++ dbscript/upgrade/v_refactor_20201215.sql
@@ -32,3 +32,11 @@ update account_user_account set customer_sync_modify_holdinfo = 1 where customer
 ALTER TABLE `dili_account`.`account_serial_record` 
 MODIFY COLUMN `customer_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户角色' AFTER `customer_name`;
 
+-- 取消必填
+ALTER TABLE `dili_account`.`account_user_account` 
+MODIFY COLUMN `usage_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '备用字段-账户用途-交易/缴费/理财/水电费预存,多个以逗号分隔' AFTER `card_exist`;
+
+ALTER TABLE `dili_account`.`account_user_account` 
+MODIFY COLUMN `hold_certificate_number` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备用-持卡人证件号' AFTER `hold_name`;
+
+MODIFY COLUMN `permissions` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '备用-使用权限(充值、提现、交费等),多个以逗号分隔' AFTER `usage_type`;
