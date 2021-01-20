18253ccf8894454d851fb215135bc1d4d0c089c8
wangbo <wangbo@diligrp.com>
2021-01-20 09:22:43 +0800

    调整副卡使用资金帐号

--- account-service/src/main/java/com/dili/account/service/impl/OpenCardServiceImpl.java
+++ account-service/src/main/java/com/dili/account/service/impl/OpenCardServiceImpl.java
@@ -79,10 +79,6 @@ public class OpenCardServiceImpl implements IOpenCardService {
 			throw BizExceptionProxy.exception("该卡{}不是主卡，操作失败!", openCardInfo.getCardNo());
 		}
 
-		// 创建资金账户
-//		FundAccountDto fundAccount = buildFundAccount(openCardInfo);
-//		Long fundAccountId = payRpcResolver.createFundAccount(fundAccount);
-
 		// 保存账户信息
 //		String accountIdStr = uidRpcResovler.bizNumberRetry(BizNoServiceType.ACCOUNT_ID, 3);
 //		Long accountId = Long.parseLong(accountIdStr);
@@ -129,14 +125,12 @@ public class OpenCardServiceImpl implements IOpenCardService {
 			throw BizExceptionProxy.exception("请刷正确的主卡!{}", openCardInfo.getParentAccountId());
 		}
 
-		// 创建资金账户
-//		FundAccountDto fundAccount = buildFundAccount(openCardInfo);
-//		Long fundAccountId = payRpcResolver.createFundAccount(fundAccount);
-
 		// 构建账户信息
-		String accountIdStr = uidRpcResovler.bizNumber(BizNoServiceType.ACCOUNT_ID);
-		log.info("编号服务获取账户ID*****{}", accountIdStr);
-		Long accountId = Long.parseLong(accountIdStr);
+//		String accountIdStr = uidRpcResovler.bizNumber(BizNoServiceType.ACCOUNT_ID);
+//		log.info("编号服务获取账户ID*****{}", accountIdStr);
+//		Long accountId = Long.parseLong(accountIdStr);
+		// 使用支付账户ID，为以后合并做准备
+		Long accountId = openCardInfo.getFundAccountId();
 		UserAccountDo userAccount = buildUserAccount(openCardInfo, accountId, openCardInfo.getFundAccountId());
 		userAccountDao.save(userAccount);
 
